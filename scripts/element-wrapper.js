06bb0ed40ee9a9091ec9b3554dfa44063fe0be88
EneaEntertainment <8155449+EneaEntertainment@users.noreply.github.com>
2019-06-19 21:38:37 +0200

    init

---
+++ scripts/element-wrapper.js
@@ -0,0 +1,169 @@
+import bounds from './bounds.js';
+
+/**
+ *
+ * ElementWrapper
+ *
+ * @version : 2.0.0
+ * @author  : http://www.enea.sk
+ *
+ * @export
+ * @class ElementWrapper
+ * @extends {PIXI.DisplayObject}
+ */
+export default class ElementWrapper extends PIXI.DisplayObject
+{
+    /**
+     * Creates an instance of ElementWrapper.
+     *
+     * @param {Element} [target=null]
+     */
+    constructor(target = null)
+    {
+        super();
+
+        this.target = target;
+
+        this.previousTransform = new PIXI.Matrix();
+
+        this._anchorX = 0;
+        this._anchorY = 0;
+    }
+
+    /**
+     *
+     * compareMatrices
+     *
+     * @param {PIXI.Matrix} matrix1
+     * @param {PIXI.Matrix} matrix2
+     * @returns {boolean}
+     */
+    compareMatrices(matrix1, matrix2)
+    {
+        return (
+            matrix1.a === matrix2.a &&
+            matrix1.b === matrix2.b &&
+            matrix1.c === matrix2.c &&
+            matrix1.d === matrix2.d &&
+            matrix1.tx === matrix2.tx &&
+            matrix1.ty === matrix2.ty
+        );
+    }
+
+    /**
+     *
+     * updateTarget
+     *
+     */
+    updateTarget()
+    {
+        const matrix = this.worldTransform;
+
+        this.target.style.left = `${bounds.x}px`;
+        this.target.style.top = `${bounds.y}px`;
+        this.target.style.transform = `matrix(${matrix.a}, ${matrix.b}, ${matrix.c}, ${matrix.d}, ${matrix.tx}, ${matrix.ty})`;
+    }
+
+    /**
+     *
+     * render
+     *
+     */
+    render()
+    {
+        if (this.compareMatrices(this.worldTransform, this.previousTransform) || this.target === null)
+        {
+            return;
+        }
+
+        this.updateTarget();
+
+        this.previousTransform = this.worldTransform.clone();
+    }
+
+    /**
+     *
+     * destroy
+     *
+     */
+    destroy()
+    {
+        this.target = null;
+
+        this.previousTransform = null;
+
+        super.destroy();
+    }
+
+    /**
+     *
+     * bounds
+     *
+     * @readonly
+     */
+    get bounds()
+    {
+        return this.target.getBoundingClientRect();
+    }
+
+    /**
+     *
+     * anchorX
+     *
+     */
+    get anchorX()
+    {
+        return this._anchorX;
+    }
+
+    /**
+     *
+     * anchorX
+     *
+     * @param {number} value
+     */
+    set anchorX(value)
+    {
+        this._anchorX = value;
+
+        this.pivot.x = value * this.bounds.width;
+    }
+
+    /**
+     *
+     * anchorY
+     *
+     */
+    get anchorY()
+    {
+        return this._anchorY;
+    }
+
+    /**
+     *
+     * anchorY
+     *
+     * @param {number} value
+     */
+    set anchorY(value)
+    {
+        this._anchorY = value;
+
+        this.pivot.y = value * this.bounds.height;
+    }
+
+    /**
+     *
+     * anchorXY
+     *
+     * @param {number} value
+     */
+    set anchorXY(value)
+    {
+        this.anchorX = value;
+        this.anchorY = value;
+    }
+}
+
+ElementWrapper.prototype.renderWebGL = ElementWrapper.prototype.render;
+ElementWrapper.prototype.renderCanvas = ElementWrapper.prototype.render;
---
+++ scripts/main.js
@@ -0,0 +1,91 @@
+import bounds from './bounds.js';
+import ElementWrapper from './element-wrapper.js';
+
+let bunny;
+let wrappedElement;
+
+// init app
+const app = new PIXI.Application({
+    width           : window.innerWidth,
+    height          : window.innerHeight,
+    autoDensity     : true,
+    backgroundColor : 0x3a7ccd
+});
+
+const canvasHolder = document.getElementById('pixiCanvas');
+
+canvasHolder.appendChild(app.view);
+
+// load texture
+app.loader.add('bunny', 'https://pixijs.io/examples/examples/assets/bunny.png');
+app.loader.load(onTextureLoaded);
+
+function onTextureLoaded(loader, resources)
+{
+    // create bunny
+    bunny = new PIXI.Sprite(resources.bunny.texture);
+
+    bunny.anchor.set(0.5);
+
+    app.stage.addChild(bunny);
+
+    // spin bunny
+    app.ticker.add((delta) =>
+    {
+        bunny.rotation += 0.1 * delta;
+    });
+
+    // get element to wrap things around
+    const element = document.getElementById('my-element');
+
+    element.addEventListener('submit', shake);
+
+    // create wrapped element
+    wrappedElement = new ElementWrapper(element);
+
+    wrappedElement.anchorXY = 0.5;
+
+    app.stage.addChild(wrappedElement);
+
+    // handle window resize
+    window.addEventListener('resize', onResize);
+
+    onResize();
+}
+
+function shake(e)
+{
+    e.preventDefault();
+
+    wrappedElement.scale.set(1.1);
+    wrappedElement.rotation = Math.random() > 0.5 ? 0.075 : -0.075;
+
+    setTimeout(() =>
+    {
+        wrappedElement.scale.set(1);
+        wrappedElement.rotation = 0;
+    }, 100);
+}
+
+function onResize()
+{
+    // resize renderer
+    const { innerWidth, innerHeight } = window;
+
+    app.renderer.resize(innerWidth, innerHeight);
+
+    // store canvas bounds
+    const rect = app.view.getBoundingClientRect();
+
+    bounds.x = rect.left + (window.scrollX || window.pageXOffset);
+    bounds.y = rect.top + (window.scrollY || window.pageYOffset);
+    bounds.width = rect.width;
+    bounds.height = rect.height;
+
+    // reposition bunny & wrapped element
+    bunny.x = bounds.width >> 1;
+    bunny.y = (bounds.height >> 1) - (wrappedElement.bounds.height >> 1) - 20;
+
+    wrappedElement.x = bounds.width >> 1;
+    wrappedElement.y = (bounds.height >> 1) + 20;
+}
